41d70293fed721e5682f22ff787d43b54b1c165b
claudivannj <claudivan.ar2002@gmail.com>
2025-08-19 03:17:21 -0300

    feat: adiciona e estiliza componente home com formulario de busca de usuário

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent {  
+    private fb = inject(FormBuilder);
+    private router = inject(Router);
 
+    form = this.fb.group({username: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9-]+$/)]]});
+
+    onSubmit():void{
+      const username = this.form.value.username?.trim();
+      if(username){
+        this.router.navigate(['/perfil', username]);
+      }
+    }
 }
