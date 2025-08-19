8e0b22919088bdad5160505d7085b885184e2d42
claudivannj <claudivan.ar2002@gmail.com>
2025-08-19 02:38:36 -0300

    feat: cria service para comunicação com API do github (usuario e repositórios)

--- src/app/services/github-api.service.ts
+++ src/app/services/github-api.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+import { Repo } from '../models/repo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubApiService {
+  private http = inject(HttpClient);
+  private baseUrl = `${environment.apiUrl}/users`;
 
-  constructor() { }
+  getUserByName(username:string):Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/${username}`);
+  }
+
+  getRepos(username:string):Observable<Repo[]> {
+    return this.http.get<Repo[]>(`${this.baseUrl}/${username}/repos`)
+  }
 }
