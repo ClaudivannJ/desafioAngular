f48ea233a1899df50f7526f324b287cd381c5ef5
claudivannj <claudivan.ar2002@gmail.com>
2025-08-19 01:45:09 -0300

    chore: inicializa projeto Angular e adiciona estrutua inicial com models (User, Repo)

---
+++ src/app/models/repo.model.ts
@@ -0,0 +1,7 @@
+export interface Repo {
+    name: string;
+    description: string;
+    stargazers_count: number;
+    updated_at: string;
+    html_url: string;
+}
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,14 @@
+export interface User {
+    login: string;
+    name: string;
+    avatar_url: string;
+    bio: string;
+    company?:string;
+    location?: string;
+    email?: string;
+    blog?: string;
+    twitter_username?: string;
+    followers: number;
+    following: number;
+    public_repos:number;
+}
