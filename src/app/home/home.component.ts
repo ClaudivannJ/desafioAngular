02e0a95e7decd443e0937be85a7aaec71644c282
claudivannj <claudivan.ar2002@gmail.com>
2025-08-20 12:07:23 -0300

    feat(home): adiciona lógica do componente para atender requisitos do desafio
    - melhora form de busca com validação correta
    - ajusta estilos para seguir protótipo do Figma
    - corrige navegação para página de perfil

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [ ReactiveFormsModule],
+  imports: [ ReactiveFormsModule, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {  
     private fb = inject(FormBuilder);
     private router = inject(Router);
+    faSearch = faSearch;
 
     form = this.fb.group({username: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9-]+$/)]]});
 
@@ -20,4 +23,5 @@ export class HomeComponent {
         this.router.navigate(['/perfil', username]);
       }
     }
+
 }
